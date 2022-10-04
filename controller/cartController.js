7498104dd0d7cd30cf21d3eacab22b95cea5769e
JamalNizami <jamal.anjum2001@gmail.com>
2022-10-04 22:59:44 +0500

    Cart Controller Modified

--- controller/cartController.js
+++ controller/cartController.js
@@ -2,12 +2,24 @@ const express = require ('express')
 const asyncHandler = require('express-async-handler')
 const Cart = require('../model/cartModel')
 
-const setCart = asyncHandler ( async (req,res) => {
-    const cart = await Cart.create(req.body)
-    res.status(200).json(cart)
+// const setCart = asyncHandler ( async (req,res) => {
+//     const cart = await Cart.create(req.body)
+//     res.status(200).json(cart)
+// })
+
+// Getting Cart of a user 
+
+const getCart = asyncHandler (async (req , res) => {
+    const cart = await Cart.find(req.body)
+    res.status(200).json({
+        Message : "This is your Cart",
+        Cart : cart,
+    })
 })
 
 
+
 module.exports = {
-    setCart,
+    // setCart,
+    getCart,
 }
