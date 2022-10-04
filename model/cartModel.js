27d80adb85e049c3b11b1ddfae7626e9d83eb840
JamalNizami <jamal.anjum2001@gmail.com>
2022-10-04 23:03:07 +0500

    Model Updated

--- model/cartModel.js
+++ model/cartModel.js
@@ -1,20 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const CartSchema = new Schema({
-    cartItem: [{
-     type: Schema.Types.ObjectId,
-     ref : 'CartItem',
-     required : true
-    }],
-
-    totalNoOfItem: {
-     type: Number,
-    },
-
-    totalPrice: {
-     type: Number,
-    },
-
+    userId : {
+        type : Schema.Types.ObjectId,
+        ref : "User",
+        require : true
+    }
   },
   {
     timestraps: true,
